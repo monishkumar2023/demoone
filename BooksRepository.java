15b76d59fc30ed6bf1b7629f46ffa6a60cca2eff
unknown <monishkumar2023>
2023-03-19 21:46:38 +0530

    copy of new

--- BooksRepository.java
+++ BooksRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.crudOperation.model.Books;
 public interface BooksRepository extends CrudRepository<Books, Integer>
 {
+welcome da my repository ku....
 }
